56177b06267160fcee65f84333bda33ba50bea0e
Suyash Gupta <Suyashgupta34604@gmail.com>
2026-07-07 10:49:08 +0530

    login and logout route completed

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 const userModel = require("../models/user.model");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken") 
 
 exports.register = async (req,res)=>{
   try{
@@ -18,3 +19,27 @@ exports.register = async (req,res)=>{
   }
 }
 
+exports.login = async(req,res)=>{
+  try {
+    const{email,password} = req.body
+    const user = await userModel.findOne({email});
+    if(!user){
+        return res.status(401).json({message:"Invalid credentials"})
+    }
+    const match = await bcrypt.compare(password,user.password)
+    if(!match){
+         return res.status(401).json({message:"Invalid credentials"})
+    }
+    const token = jwt.sign({_id:user._id},process.env.JWT_SECRET,{expiresIn:"7d"})
+    res.cookie("token",token,{httpOnly:true,secure:true,sameSite:"strict",maxAge:24*60*60*1000})
+    res.status(201).json({success:true,message:"Login successful",token})
+
+  } catch (err) {
+     console.error(err.message)
+  }
+}
+
+exports.logout = async(req,res)=>{
+    res.clearCookie("token")
+    res.status(200).json({message:"Logout successful"})
+}
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -4,5 +4,7 @@ const authController = require("../controllers/auth.controller.js");
 const {register,login,logout} = authController;
 
 router.post("/register",register); 
+router.post("/login",login);
+router.post("/logout",logout);
 
 module.exports = router;
