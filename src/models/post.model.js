4ab155ef8d0914d809fb6774ea632d45be62fc78
Suyash Gupta <Suyashgupta34604@gmail.com>
2026-07-07 11:29:03 +0530

    data association and image tool kit install

---
+++ src/models/post.model.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+const mongoose = require("mongoose")
+const postSchema = new mongoose.Schema({
+    image:String,
+    caption:String,
+    owner:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    }
+})
+const post = mongoose.model("Post",postSchema);
+module.exports = post;
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -3,6 +3,10 @@ const userSchema = new mongoose.Schema({
   username:{type:String, required:true},  
   email:{type:String, required:true, unique:true},
   password:{type:String, required:true},
+  posts:[{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:"Post"
+  }]
   
 })
 
