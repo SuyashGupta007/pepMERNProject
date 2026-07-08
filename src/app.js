1a63271085b01276da65f4da5767c0bd67fcba65
Suyash Gupta <Suyashgupta34604@gmail.com>
2026-07-08 11:43:21 +0530

    feat: initialize express app with auth and post route configurations

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 const express = require("express")
 const app = express();
 const authRoutes = require("./routes/auth.routes")
-const postRoutes = require("./routes/post.routes");
-const cookieParser = require("cookie-parser");
+const postRoutes = require("./routes/post.routes")
+const cookieParser = require("cookie-parser")
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({extended:true}));
